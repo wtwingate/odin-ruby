811739b87e528d0dcf90cdc2248d09f60579ad28
William Wingate <wtwingate@gmail.com>
2025-02-28 11:28:17 -0500

    Configure RuboCop and apply corrections

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -2,10 +2,10 @@
 
 def caesar_cipher(message, shift)
   message.chars.map do |char|
-    if char =~ /[a-z]/
+    if /[a-z]/.match?(char)
       base = 'a'.ord
       (base + ((char.ord - base + shift) % 26)).chr
-    elsif char =~ /[A-Z]/
+    elsif /[A-Z]/.match?(char)
       base = 'A'.ord
       (base + ((char.ord - base + shift) % 26)).chr
     else
