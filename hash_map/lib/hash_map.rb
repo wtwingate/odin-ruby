5ab3dda2e3d130fef867e7999ae426804847c2c3
William Wingate <wtwingate@gmail.com>
2025-03-18 15:23:16 -0400

    Implement more methods for the HashMap class

--- hash_map/lib/hash_map.rb
+++ hash_map/lib/hash_map.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 # This class implements a Hash Map data structure.
+# rubocop: disable Metrics/ClassLength
 class HashMap
   attr_accessor :size, :capacity
-  
+
   def initialize
     @size = 0
     @load_factor = 0.75
@@ -11,8 +12,10 @@ class HashMap
     @buckets = Array.new(@capacity)
   end
 
-  # rubocop: disable Metrics/MethodLength
+  # rubocop: disable Metrics/MethodLength, Metrics/AbcSize
   def set(key, value)
+    grow if @size.to_f / @capacity >= @load_factor
+
     index = hash(key)
 
     # no hash collision
@@ -43,7 +46,7 @@ class HashMap
 
     value
   end
-  # rubocop: enable Metrics/MethodLength
+  # rubocop: enable Metrics/MethodLength, Metrics/AbcSize
 
   def get(key)
     index = hash(key)
@@ -61,6 +64,79 @@ class HashMap
     nil
   end
 
+  def has?(key)
+    !!get(key)
+  end
+
+  # rubocop: disable Metrics/MethodLength
+  def remove(key)
+    index = hash(key)
+
+    return nil if @buckets[index].nil?
+
+    prev = nil
+    current = @buckets[index]
+
+    until current.nil?
+      unless current.key == key
+        prev = current
+        current = current.next
+        next
+      end
+
+      if prev.nil? # first node in bucket
+        @buckets[index] = current.next
+      else
+        prev.next = current.next
+      end
+
+      @size -= 1
+      return current.value
+    end
+
+    nil
+  end
+  # rubocop: enable Metrics/MethodLength
+
+  def length
+    @size
+  end
+
+  def clear
+    @size = 0
+    @capacity = 16
+    @buckets = Array.new(@capacity)
+  end
+
+  def entries
+    data = []
+
+    @buckets.each do |bucket|
+      next if bucket.nil?
+
+      current = bucket
+
+      until current.nil?
+        data << [current.key, current.value]
+        current = current.next
+      end
+    end
+
+    data
+  end
+
+  def keys
+    data = entries
+
+    data.map { |key, _value| key }
+  end
+
+  def values
+    data = entries
+
+    data.map { |_key, value| value }
+  end
+
   private
 
   def hash(key)
@@ -73,4 +149,17 @@ class HashMap
 
     hash_code % @capacity
   end
+
+  def grow
+    data = entries
+
+    @size = 0
+    @capacity *= 2
+    @buckets = Array.new(@capacity)
+
+    data.each do |key, value|
+      set(key, value)
+    end
+  end
 end
+# rubocop: enable Metrics/ClassLength
--- hash_map/main.rb
+++ hash_map/main.rb
@@ -5,28 +5,43 @@ require_relative 'lib/node'
 
 hash_map = HashMap.new
 
-p hash_map.set('duck', 42)
-p hash_map.set('goose', 42)
-p hash_map.set('pig', 42)
-p hash_map.set('boar', 42)
-p hash_map.set('dog', 42)
-p hash_map.set('cat', 42)
-p hash_map.set('parrot', 42)
-p hash_map.set('hamster', 42)
-p hash_map.set('snake', 42)
-p hash_map.set('fish', 42)
-p hash_map.set('lizard', 42)
-p hash_map.set('frog', 42)
-p hash_map.set('ferret', 42)
-p hash_map.set('monkey', 42)
-p hash_map.set('chicken', 42)
-p hash_map.set('cow', 42)
-p hash_map.set('goat', 42)
+hash_map.set('duck', 42)
+hash_map.set('goose', 42)
+hash_map.set('pig', 42)
+hash_map.set('boar', 42)
+hash_map.set('dog', 42)
+hash_map.set('cat', 42)
+hash_map.set('parrot', 42)
+hash_map.set('hamster', 42)
+
+p hash_map.size
+p hash_map.capacity
+
+hash_map.set('snake', 42)
+hash_map.set('fish', 42)
+hash_map.set('lizard', 42)
+hash_map.set('frog', 42)
+hash_map.set('ferret', 42)
+hash_map.set('monkey', 42)
+hash_map.set('chicken', 42)
+hash_map.set('cow', 42)
 
 p hash_map.get('duck')
 
-p hash_map.set('duck', 69)
+hash_map.set('duck', 69)
 
 p hash_map.get('duck')
 
 p hash_map.size
+p hash_map.capacity
+
+p hash_map.entries
+p hash_map.keys
+p hash_map.values
+
+p hash_map.has?('goose')
+p hash_map.remove('goose')
+p hash_map.has?('goose')
+
+hash_map.clear
+p hash_map.entries
