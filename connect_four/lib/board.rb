bbf241833b761a34f30ec98805238fe277086da5
William Wingate <wtwingate@gmail.com>
2025-03-31 22:27:37 -0400

    Begin writing tests for win condition

--- connect_four/lib/board.rb
+++ connect_four/lib/board.rb
@@ -1,8 +1,17 @@
 #frozen_string_literal: true
 
 class Board
+  COLUMNS = 7
+  ROWS = 6
+  LINES = [
+    [[0, 1], [0, 2], [0, 3]],
+    [[1, 0], [2, 0], [3, 0]],
+    [[1, 1], [2, 2], [3, 3]],
+    [[1, -1], [2, -2], [3, -3]]
+  ].freeze
+  
   def initialize
-    @grid = Array.new(7) { Array.new(6) }
+    @grid = Array.new(COLUMNS) { Array.new(ROWS) }
   end
 
   def full?
@@ -11,5 +20,42 @@ class Board
 
   def column_full?(index)
     @grid[index].none? nil
+  end
+
+  def four_in_a_row?
+    @grid.each_with_index do |column, x|
+      column.each_with_index do |square, y|
+        next if square.nil?
+        
+        return true if on_some_line?(x, y)
+      end
+    end
+
+    false
+  end
+
+  private
+
+  def on_some_line?(x, y)
+    LINES.each do |line|
+      winning_line = true
+      line.each do |dx, dy|
+        nx = x + dx
+        ny = y + dy
+
+        if off_the_grid?(nx, ny) || @grid[x][y] != @grid[nx][ny]
+          winning_line = false
+          break
+        end
+      end
+
+      return true if winning_line
+    end
+
+    false
+  end
+
+  def off_the_grid?(x, y)
+    x < 0 || x >= COLUMNS || y < 0 || y >= ROWS
   end
 end
--- connect_four/spec/board_spec.rb
+++ connect_four/spec/board_spec.rb
@@ -1,75 +1,176 @@
+# frozen_string_literal: true
+
 require_relative '../lib/board'
 
+# rubocop: disable Metrics
 describe Board do
-  describe "#full?" do
+  describe '#full?' do
     subject(:board) { described_class.new }
     let(:token) { :foo }
-    
-    context "when the grid is full" do
+
+    context 'when the grid is full' do
       before do
         grid = Array.new(7) { Array.new(6) { token } }
         board.instance_variable_set(:@grid, grid)
       end
-      
-      it "returns true" do
+
+      it 'returns true' do
         expect(board).to be_full
       end
     end
 
-    context "when the grid is empty" do
+    context 'when the grid is empty' do
       before do
         grid = Array.new(7) { Array.new(6) }
-        board.instance_variable_set(:@grid, grid)        
+        board.instance_variable_set(:@grid, grid)
       end
-      
-      it "returns false" do
+
+      it 'returns false' do
         expect(board).not_to be_full
       end
     end
 
-    context "when the grid is almost full" do
+    context 'when the grid is almost full' do
       before do
         grid = Array.new(7) { Array.new(6) { token } }
         grid[0][0] = nil
         board.instance_variable_set(:@grid, grid)
       end
-      
-      it "returns false" do
+
+      it 'returns false' do
         expect(board).not_to be_full
       end
     end
 
-    context "when the grid is almost empty" do
+    context 'when the grid is almost empty' do
       before do
         grid = Array.new(7) { Array.new(6) }
         grid[0][0] = token
-        board.instance_variable_set(:@grid, grid)        
+        board.instance_variable_set(:@grid, grid)
       end
-      
-      it "returns false" do
+
+      it 'returns false' do
         expect(board).not_to be_full
       end
     end
   end
 
-  describe "#column_full?" do
+  describe '#column_full?' do
     subject(:board) { described_class.new }
     let(:token) { :foo }
     let(:index) { 0 }
 
-    context "when the specified column is full" do
+    context 'when the specified column is full' do
       before do
         grid = Array.new(7) { Array.new(6) }
         grid[index] = Array.new(6) { token }
         board.instance_variable_set(:@grid, grid)
       end
-      
-      it "returns true" do
+
+      it 'returns true' do
         expect(board.column_full?(index)).to be true
+      end
+    end
+
+    context 'when the specified column is empty' do
+      before do
+        grid = Array.new(7) { Array.new(6) }
+        board.instance_variable_set(:@grid, grid)
+      end
+
+      it 'returns false' do
+        expect(board.column_full?(index)).to be false
+      end
+    end
+
+    context 'when the specified column is almost full' do
+      before do
+        grid = Array.new(7) { Array.new(6) }
+        grid[index] = [token, token, token, token, token, nil]
+        board.instance_variable_set(:@grid, grid)
+      end
+
+      it 'returns false' do
+        expect(board.column_full?(index)).to be false
+      end
+    end
+
+    context 'when the specified column is almost empty' do
+      before do
+        grid = Array.new(7) { Array.new(6) }
+        grid[index] = [token, nil, nil, nil, nil, nil]
+        board.instance_variable_set(:@grid, grid)
+      end
+
+      it 'returns false' do
+        expect(board.column_full?(index)).to be false
       end
     end
   end
 
-  describe "#four_in_a_row?" do
+  describe '#four_in_a_row?' do
+    subject(:board) { described_class.new }
+    let(:token) { :foo }
+
+    context 'when there are four tokens in a horizontal line' do
+      before do
+        grid = Array.new(7) { Array.new(6) }
+        grid[0] = [token, nil, nil, nil, nil, nil]
+        grid[1] = [token, nil, nil, nil, nil, nil]
+        grid[2] = [token, nil, nil, nil, nil, nil]
+        grid[3] = [token, nil, nil, nil, nil, nil]
+        board.instance_variable_set(:@grid, grid)
+      end
+
+      it 'returns true' do
+        expect(board).to be_four_in_a_row
+      end
+    end
+
+    context 'when there are four tokens in a vertical line' do
+      before do
+        grid = Array.new(7) { Array.new(6) }
+        grid[0] = [token, token, token, token, nil, nil]
+        board.instance_variable_set(:@grid, grid)
+      end
+
+      it 'returns true' do
+        expect(board).to be_four_in_a_row
+      end
+    end
+
+    context 'when there are four tokens in a positive diagonal line' do
+      before do
+        grid = Array.new(7) { Array.new(6) }
+        grid[0] = [token, nil, nil, nil, nil, nil]
+        grid[1] = [nil, token, nil, nil, nil, nil]
+        grid[2] = [nil, nil, token, nil, nil, nil]
+        grid[3] = [nil, nil, nil, token, nil, nil]
+        board.instance_variable_set(:@grid, grid)
+      end
+
+      it 'returns true' do
+        expect(board).to be_four_in_a_row
+      end
+    end
+
+    context 'when there are four tokens in a negative diagonal line' do
+      before do
+        grid = Array.new(7) { Array.new(6) }
+        grid[0] = [nil, nil, nil, token, nil, nil]
+        grid[1] = [nil, nil, token, nil, nil, nil]
+        grid[2] = [nil, token, nil, nil, nil, nil]
+        grid[3] = [token, nil, nil, nil, nil, nil]
+        board.instance_variable_set(:@grid, grid)
+      end
+
+      it 'returns true' do
+        expect(board).to be_four_in_a_row
+      end
+    end
+  end
+
+  describe '#place_token' do
   end
 end
+# rubocop: enable Metrics
